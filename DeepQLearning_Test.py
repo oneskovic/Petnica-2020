515045c598dfc5340ffaef81db3aed0c528ca47a
Ognjen Neskovic <oneskovic0@gmail.com>
2020-08-31 16:09:43 +0200

    Complete game implementation
    Game environment:
    Implement the entirety of the game - 3 types of organisms, reproduction, etc.
    Fix bugs in the environment (proper order of moves, make sure games end within 200 moves...)
    Refactor code
    
    Training algorithm:
    Fix bugs in training and computation of average return
    Add additional hyperparameters (epsilon, neural net update frequency, temporal difference sample size)
    Use an epsilon-greedy strategy when collecting new data

---
+++ DeepQLearning_Test.py
@@ -0,0 +1,305 @@
+from __future__ import absolute_import, division, print_function
+
+import base64
+import json
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL.Image
+
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+from tf_agents.environments import utils
+from tf_agents.policies.epsilon_greedy_policy import EpsilonGreedyPolicy
+from game_environment2D import GameEnv
+
+num_iterations = 20000 # @param {type:"integer"}
+
+initial_collect_steps = 1000  # @param {type:"integer"} 
+collect_steps_per_iteration = 2  # @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 32  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200 # @param {type:"integer"}
+
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
+
+start_epsilon = 1
+end_epsilon = 0.1
+epsilon_anneal_steps = 4000
+
+nn_update_frequency = 4
+td_sample_size = 4
+
+def collect_step(environment, policy, buffer):
+  time_step = environment.current_time_step()
+  action_step = policy.action(time_step)
+  next_time_step = environment.step(action_step.action)
+  traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+  # Add trajectory to the replay buffer
+  buffer.add_batch(traj)
+
+def collect_data(env, policy, buffer, steps):
+  for _ in range(steps):
+    collect_step(env, policy, buffer)
+    
+def plot_organisms(organisms,cmap):
+  x_positions = []
+  y_positions = []
+  hps = []
+  for i in range(len(organisms)):
+    x_positions.append(organisms[i][0])
+    y_positions.append(organisms[i][1])
+    hps.append(organisms[i][2])
+  plt.scatter(x_positions, y_positions,c = hps,cmap=cmap,vmin=0,vmax=20)
+  
+def plot_state(state,directory):
+  i = 0
+  red_organisms = []
+  blue_organisms = []
+  green_organisms = []
+  while i < len(state):
+    x = state[i]
+    y = state[i+1]
+    hp = state[i+2]
+    type = state[i+4]
+    if type == 0:
+      green_organisms.append([x,y,hp])
+    elif type == 1:
+      blue_organisms.append([x,y,hp])
+    else:
+      red_organisms.append([x,y,hp])
+    i += 5
+  plt.cla()
+  plt.xlim(0,10)
+  plt.ylim(0,10)
+  plot_organisms(red_organisms,'Reds')
+  plot_organisms(blue_organisms,'Blues')
+  plot_organisms(green_organisms,'Greens')
+  plt.savefig(directory)
+  plt.cla()
+    
+picture_count = 0
+episode_count = 0
+
+def compute_avg_return(environment, policy, num_episodes=10, display_moves = False):
+
+  if display_moves:
+    global episode_count
+    episode_path = 'results/DeepQ-2/game-drawn/episode-'+str(episode_count)
+    if not os.path.exists(episode_path):
+      os.makedirs(episode_path)
+    
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    global picture_count
+    while not time_step.is_last():
+      observation = time_step.observation.numpy()[0]
+      plot_state(observation,episode_path+'/'+str(picture_count)+'.jpeg')
+      picture_count += 1
+      #print(time_step.observation.numpy())
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    print('Episode return: '+str(episode_return))
+    picture_count = 0
+    episode_count += 1
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+    
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+def solve_perfectly(environment, num_episodes=10):
+    total_return = 0.0
+    for _ in range(num_episodes):
+      time_step = environment.reset()
+      while not time_step.is_last():
+        dx = time_step.observation.numpy()[0][0] - time_step.observation.numpy()[0][3]
+        action = np.array(0, dtype = np.int32)
+        if abs(dx) > 0:
+            action = np.array(0, dtype=np.int32)
+            if dx < 0:
+              action = np.array(1, dtype=np.int32)
+        else:
+            dy = time_step.observation.numpy()[0][1] - time_step.observation.numpy()[0][4]
+            action = np.array(3, dtype=np.int32)
+            if dy < 0:
+                action = np.array(2, dtype=np.int32)
+
+        time_step = environment.step(action)
+        total_return += time_step.reward
+
+    return total_return.numpy()[0] / num_episodes
+losses = [] # losses will contain losses generated in each iteration
+
+def try_hparams(hparams):
+
+    # Initialize train and eval environments
+    environment = GameEnv()
+    utils.validate_py_environment(environment, episodes=5)
+    train_py_env = GameEnv()
+    eval_py_env = GameEnv()
+    train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+    eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+    # Initialize the QNetwork
+    fc_layer_params = (hparams['layer1_count'],hparams['layer2_count'],)#hparams['layer3_count'],)
+    q_net = q_network.QNetwork(
+        train_env.observation_spec(),
+        train_env.action_spec(),
+        fc_layer_params=fc_layer_params)
+
+    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+    #optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=0.9, momentum=0.95, epsilon=1e-07)
+
+    train_step_counter = tf.Variable(0)
+
+    # Initialize the DQN Agent
+    agent = dqn_agent.DqnAgent(
+        train_env.time_step_spec(),
+        train_env.action_spec(),
+        q_network=q_net,
+        optimizer=optimizer,
+        n_step_update=td_sample_size,
+        target_update_period=nn_update_frequency,
+        td_errors_loss_fn=common.element_wise_squared_loss,
+        train_step_counter=train_step_counter)
+
+    agent.initialize()
+
+    replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+        data_spec=agent.collect_data_spec,
+        batch_size=train_env.batch_size,
+        max_length=replay_buffer_max_length)
+
+    # Collect some data using a totaly random policy
+    random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
+                                                    train_env.action_spec())
+    collect_data(train_env, random_policy, replay_buffer, steps=initial_collect_steps)
+
+    # Convert replay buffer to dataset
+    dataset = replay_buffer.as_dataset(
+        num_parallel_calls=3, 
+        sample_batch_size=batch_size, 
+        num_steps=td_sample_size+1).prefetch(3)
+
+    iterator = iter(dataset)
+
+    agent.train = common.function(agent.train)
+    # Reset the train step
+    agent.train_step_counter.assign(0)
+
+    # Evaluate the agents policy, random policy and optimal policy once before training
+    optimal_return = 0 #solve_perfectly(eval_env,num_eval_episodes)
+    random_return = compute_avg_return(eval_env,random_policy,num_eval_episodes)
+    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+    
+    returns = [avg_return] # returns will contain all average returns of the agent during training
+    global losses
+    epsilon = start_epsilon
+    epsilon_step = (start_epsilon-end_epsilon) / epsilon_anneal_steps
+
+    for _ in range(num_iterations):
+      # Reduce epsilon
+      epsilon = max(epsilon - epsilon_step, end_epsilon)
+        
+      # Collect a few steps using the epsilon greedy policy and save to the replay buffer.
+      for _ in range(collect_steps_per_iteration):
+        collect_step(train_env, EpsilonGreedyPolicy(agent.policy, epsilon), replay_buffer)
+
+      # Sample a batch of data from the buffer and update the agent's network.
+      experience, unused_info = next(iterator)
+      train_loss = agent.train(experience).loss
+
+      step = agent.train_step_counter.numpy()
+
+      if step % log_interval == 0:
+        print('step = {0}: loss = {1}'.format(step, train_loss))
+        losses.append(train_loss)
+        
+      if step % eval_interval == 0:
+        avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes, True)
+        print('step = {0}: Average Return = {1} Optimal policy = {2} Random policy = {3}'.format(step, avg_return, optimal_return,random_return))
+        returns.append(avg_return)
+    
+    #plt.plot(losses)
+    #plt.show()
+    #plt.cla()
+    return returns
+
+layer1_counts = [100]
+layer2_counts = [20]
+
+import string
+import random
+def generate_random_string(length):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+for count1 in layer1_counts:
+    for count2 in layer2_counts:
+        hparams = {
+            'layer1_count': count1,
+            'layer2_count': count2
+            }
+        results = try_hparams(hparams)
+        file_name = generate_random_string(6)
+        result_file = open('results/DeepQ-2/'+str(file_name)+'.json','w+')
+        hparams_map = {
+          'num_iterations':num_iterations,
+          'initial_collect_steps': initial_collect_steps, 
+          'collect_steps_per_iteration': collect_steps_per_iteration,
+          'replay_buffer_max_length': replay_buffer_max_length,
+
+          'batch_size': batch_size,
+          'learning_rate': learning_rate,
+          'log_interval': log_interval,
+
+          'num_eval_episodes': num_eval_episodes,
+          'eval_interval': eval_interval,
+
+          'start_epsilon': start_epsilon,
+          'end_epsilon': end_epsilon,
+          'epsilon_anneal_steps': epsilon_anneal_steps,
+
+          'nn_update_frequency': nn_update_frequency,
+          'td_sample_size': td_sample_size,
+          'nn_architecture': (count1,count2)
+        }
+        json.dump(hparams_map,result_file,sort_keys=True, indent=4)
+        result_file.close()
+        plt.cla()
+        plt.plot(losses)
+        plt.savefig('results/DeepQ-2/Loss-'+str(file_name))
+        plt.cla()
+        plt.plot(results)
+        plt.savefig('results/DeepQ-2/Score-'+str(file_name))
+        plt.cla()
---
+++ game_environment2D.py
@@ -0,0 +1,299 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import tensorflow as tf
+import numpy as np
+from copy import deepcopy
+
+from tf_agents.environments import py_environment
+from tf_agents.environments import tf_environment
+from tf_agents.environments import tf_py_environment
+from tf_agents.environments import utils
+from tf_agents.specs import array_spec
+from tf_agents.environments import wrappers
+from tf_agents.environments import suite_gym
+from tf_agents.trajectories import time_step as ts
+
+tf.compat.v1.enable_v2_behavior()
+
+class Organism:
+    time_alive = 0
+    time_to_reproduce = 0
+    x_pos = 0
+    y_pos = 0
+    energy = 0
+    id = -1
+    type = -1
+
+    def __init__(self, x_position, y_position, energy, type, time_to_reproduce):
+      self.x_pos = x_position
+      self.y_pos = y_position
+      self.energy = energy
+      self.type = type
+      self.time_to_reproduce = time_to_reproduce
+      self.time_alive = 0
+      self.id = np.random.randint(-100000000,100000000)
+
+    def to_list(self):
+      return [self.x_pos,self.y_pos,self.energy,self.time_to_reproduce,self.type]
+
+class GameEnv(py_environment.PyEnvironment):
+  # Lists containing the currently alive organisms
+  blue_organisms = []
+  red_organisms = []
+
+  green_organisms = []
+  start_hp = 20 # HP / energy that all organisms have when the game starts
+  board_length = 8 # Board will be of size board_length x board_length
+  food_energy = 10 # Energy that each green organism will have
+  max_moves = 200 # The maximum number of moves a game can last
+  current_move_number = 0
+  board_food_count = 10 # The number of green organisms always present on the board
+  blue_organisms_start_count = 5
+  red_organisms_start_count = 2
+  input_layer_count = 17
+  reproduction_cooldown = 3
+  
+  organisms_to_move = []
+  player_to_move = 'Red'
+
+  def __init__(self):
+    np.random.seed(12345)
+    tf.random.set_seed(12345)
+    
+    # Initialize specs
+    self._action_spec = array_spec.BoundedArraySpec(
+        shape=(), dtype=np.int32, minimum=0, maximum=3, name='action')
+    self._observation_spec = array_spec.BoundedArraySpec(
+        shape=(5*(self.input_layer_count+1),), dtype=np.int32, minimum=0, name='observation')
+
+    self._episode_ended = False
+
+    self.blue_organisms = []
+    # Initialize blue organisms
+    for _ in range(self.blue_organisms_start_count):
+        x_pos = np.random.randint(0,self.board_length)
+        y_pos = np.random.randint(0,self.board_length)
+        self.blue_organisms.append(Organism(x_pos,y_pos,self.start_hp,1,self.reproduction_cooldown))
+    
+    self.red_organisms = []
+    # Initialize red organisms
+    for _ in range(self.red_organisms_start_count):
+        x_pos = np.random.randint(0,self.board_length)
+        y_pos = np.random.randint(0,self.board_length)
+        self.red_organisms.append(Organism(x_pos,y_pos,self.start_hp,2,self.reproduction_cooldown))
+
+    self.green_organisms = []
+    # Generate green organisms
+    for _ in range(self.board_food_count):
+        x_pos = np.random.randint(0,self.board_length)
+        y_pos = np.random.randint(0,self.board_length)
+
+        self.green_organisms.append(Organism(x_pos,y_pos,self.food_energy,0,0))
+
+    self.next_id_to_move = self.blue_organisms[0].id
+    self.organism_to_move = self.blue_organisms[0]
+    self.player_to_move = 'Blue'
+    self.current_move_number = 0
+
+  def action_spec(self):
+    return self._action_spec
+
+  def observation_spec(self):
+    return self._observation_spec
+
+  def organisms_to_array(self, organisms_list, required_count):
+    organism_array = []
+    # Filter out the organism that should move next (appended to the front)
+    organisms_list = [organism for organism in organisms_list if organism.id != self.organism_to_move.id]
+    
+    # Attempt to add the required_count of organisms
+    for i in range(int(min(required_count,len(organisms_list)))):
+        organism_array += organisms_list[i].to_list()
+    
+    # If the required_count has not been reached fill the rest with 0 elements
+    for _ in range(int(max(0,required_count - len(organisms_list)))):
+        organism_array += [0]*5
+    return organism_array
+
+  def __get_distance(self, organism1, organism2):
+    dx = organism1.x_pos - organism2.x_pos
+    dy = organism1.y_pos - organism2.y_pos
+    return abs(dx)+abs(dy)
+
+  # Sorts organisms by their distance from target_organism
+  def __sort_organisms_by_distance(self, organisms, target_organism):
+    distance_dict = {}
+    # Should append to list not overwrite
+    for organism in organisms:
+      # Dirty, temporary fix
+      noise = np.random.uniform(0.0000000001,0.00001)
+      distance_dict[self.__get_distance(organism, target_organism)+noise] = organism
+    
+    ordered_organisms = []
+    for distance in sorted(distance_dict.keys()):
+      current_organism = distance_dict[distance]
+      ordered_organisms.append(current_organism)
+    return ordered_organisms
+    
+  def __get_current_game_state(self):
+    state = []
+    state += self.organisms_to_move[0].to_list() # Append the organism that should move to the front
+    
+    ordered_blues = self.__sort_organisms_by_distance(self.blue_organisms,self.organism_to_move)
+    state += self.organisms_to_array(ordered_blues, self.blue_organisms_start_count)#self.input_layer_count/3)
+    ordered_reds = self.__sort_organisms_by_distance(self.red_organisms,self.organism_to_move)
+    state += self.organisms_to_array(ordered_reds,  self.red_organisms_start_count)
+    ordered_greens = self.__sort_organisms_by_distance(self.green_organisms,self.organism_to_move)
+    state += self.organisms_to_array(ordered_greens, self.board_food_count)
+    return state
+
+  def _reset(self):
+    self._episode_ended = False
+
+    self.blue_organisms = []
+    for _ in range(self.blue_organisms_start_count):
+        x_pos = np.random.randint(0,self.board_length)
+        y_pos = np.random.randint(0,self.board_length)
+        self.blue_organisms.append(Organism(x_pos,y_pos,self.start_hp,1,self.reproduction_cooldown))
+    
+    self.red_organisms = []
+    for _ in range(self.red_organisms_start_count):
+        x_pos = np.random.randint(0,self.board_length)
+        y_pos = np.random.randint(0,self.board_length)
+        self.red_organisms.append(Organism(x_pos,y_pos,self.start_hp,2,self.reproduction_cooldown))
+
+    self.green_organisms = []
+    # Generate food
+    for _ in range(self.board_food_count):
+        x_pos = np.random.randint(0,self.board_length)
+        y_pos = np.random.randint(0,self.board_length)
+
+        self.green_organisms.append(Organism(x_pos,y_pos,self.food_energy,0,0))
+        
+    self.organisms_to_move = deepcopy(self.blue_organisms)
+    self.player_to_move = 'Blue'
+    self.current_move_number = 0
+
+    self._state = self.__get_current_game_state()
+    return ts.restart(np.array(self._state, dtype=np.int32))
+
+  def __update_organism_position(self, organism, action):
+        organism_copy = deepcopy(organism)
+        # Process organism's action
+        if action == 0:
+            organism_copy.x_pos = (organism.x_pos-1+self.board_length)%self.board_length
+        elif action == 1:
+            organism_copy.x_pos = (organism.x_pos+1)%self.board_length
+        elif action == 2:
+            organism_copy.y_pos = (organism.y_pos+1)%self.board_length
+        elif action == 3:
+            organism_copy.y_pos = (organism.y_pos-1+self.board_length)%self.board_length
+        else:
+            raise ValueError('`action` should be in range [0,3].')
+        return organism_copy
+    
+  def __move_organism(self, action, other_player, current_organisms, other_organisms):
+      i = 0
+      while i < len(self.organisms_to_move):
+        found = False
+        for organism_index in range(len(current_organisms)):
+          if self.organisms_to_move[i].id == current_organisms[organism_index].id:
+            current_organisms[organism_index] = self.__update_organism_position(current_organisms[organism_index], action)
+            current_organisms[organism_index].energy -= 1
+            current_organisms[organism_index].time_to_reproduce = \
+              max(0,current_organisms[organism_index].time_to_reproduce-1)
+            found = True
+            break
+        i+=1
+        if found:
+          break
+        
+      if i == len(self.organisms_to_move):
+        self.player_to_move = other_player
+        self.current_move_number += 1
+        self.organisms_to_move = deepcopy(other_organisms)
+      else:    
+        self.organisms_to_move = self.organisms_to_move[i:]
+  
+  # Checks if any prey should be consumed, adds energy to the adequate predator organism
+  # Sets energy = 0 for any prey that is consumed - dead prey must be removed afterwards
+  def __consume_prey(self,predator_organisms, prey_organisms):
+    for i in range(len(prey_organisms)):
+        prey = prey_organisms[i]
+        for j in range(len(predator_organisms)):
+            predator = predator_organisms[j]
+            if predator.x_pos == prey.x_pos and predator.y_pos == prey.y_pos:
+                predator_organisms[j].energy += prey.energy
+                prey_organisms[i].energy = 0
+                break
+    
+  def __reproduce_organisms(self,organisms):
+    for i in range(len(organisms)):
+      for j in range(len(organisms)):
+        if i != j:
+          if organisms[i].x_pos == organisms[j].x_pos and \
+            organisms[i].y_pos == organisms[j].y_pos and \
+            organisms[i].time_to_reproduce <= 0 and organisms[j].time_to_reproduce <= 0:
+            energy = organisms[i].energy/2 + organisms[j].energy/2
+            organisms[i].energy /= 2
+            organisms[j].energy /= 2
+            organisms[i].time_to_reproduce = self.reproduction_cooldown
+            organisms[j].time_to_reproduce = self.reproduction_cooldown
+            new_organism = Organism(organisms[i].x_pos,organisms[j].y_pos,
+                            energy,organisms[i].type,self.reproduction_cooldown)
+            organisms.append(new_organism)
+  
+  
+  def _step(self, action):
+    reward = 1
+        # Make sure episodes don't go on forever.
+    if self.current_move_number >= self.max_moves or len(self.blue_organisms) == 0 or len(self.red_organisms) == 0:
+        self._episode_ended = True
+    if self._episode_ended:
+      # The last action ended the episode. Ignore the current action and start
+      # a new episode.
+      return self.reset()
+
+    if self.player_to_move == 'Blue':
+      self.__move_organism(action,'Red',self.blue_organisms,self.red_organisms)
+    else:
+      self.__move_organism(action,'Blue',self.red_organisms,self.blue_organisms)
+    
+    # Remove dead blue organisms
+    self.blue_organisms = [organism for organism in self.blue_organisms if organism.energy > 0]
+    # Remove dead red organisms
+    self.red_organisms = [organism for organism in self.red_organisms if organism.energy > 0]
+
+    # Make sure episodes don't go on forever.
+    if self.current_move_number >= self.max_moves or len(self.blue_organisms) == 0 or len(self.red_organisms) == 0:
+        self._episode_ended = True
+        #reward = -20
+    else:    
+        self.__consume_prey(self.blue_organisms,self.green_organisms)
+        self.__consume_prey(self.red_organisms,self.blue_organisms)
+        self.__reproduce_organisms(self.blue_organisms)
+        self.__reproduce_organisms(self.red_organisms)
+
+        # Remove dead green and blue organisms. This must be done again 
+        # for green and blue as additional organisms might have been consumed
+        self.green_organisms = [organism for organism in self.green_organisms if organism.energy > 0]            
+        self.blue_organisms = [organism for organism in self.blue_organisms if organism.energy > 0]            
+    
+        if self.current_move_number >= self.max_moves or len(self.blue_organisms) == 0 or len(self.red_organisms) == 0:
+          self._episode_ended = True
+    
+        # Generate new green organisms
+        while len(self.green_organisms) != self.board_food_count:
+            x_pos = np.random.randint(0,self.board_length)
+            y_pos = np.random.randint(0,self.board_length)
+            self.green_organisms.append(Organism(x_pos,y_pos,self.food_energy,0,0))
+
+    self._state = self.__get_current_game_state()
+    if self._episode_ended:
+      return ts.termination(np.array(self._state, dtype=np.int32), reward)
+    else:
+      return ts.transition(
+          np.array(self._state, dtype=np.int32), reward=reward, discount=0.9)
