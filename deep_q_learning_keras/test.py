83f3aa7d21a11a2a7807cc25b4747e1bc995b476
Ognjen <oneskovic0@gmail.com>
2020-10-09 21:12:20 +0200

    Graphic visualization
    Add option to render the game graphically

--- deep_q_learning_keras/test.py
+++ deep_q_learning_keras/test.py
@@ -3,23 +3,25 @@ from game_env_gym import GameEnv
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines import DQN
+import time
 
 
 env = GameEnv()
 #env = gym.make('CartPole-v1')
 
-model = DQN(MlpPolicy, env, verbose=1)
+model = DQN(MlpPolicy, env, verbose=1, tensorboard_log ='tb_log')
 model.learn(total_timesteps=10000000)
 model.save("deepqrcina")
 
-# model = DQN.load('deepqrcina')
+model = DQN.load('deepqrcina')
 
-# obs = env.reset()
-# done = False
-# total_reward = 0.0
-# while not done:
-#     action, _states = model.predict(obs)
-#     obs, reward, done, info = env.step(action)
-#     total_reward += reward
-#     env.render()
-# print(total_reward)
+obs = env.reset()
+done = False
+total_reward = 0.0
+while not done:
+    action, _states = model.predict(obs)
+    obs, reward, done, info = env.step(action)
+    total_reward += reward
+    env.render()
+    #time.sleep(2)
+print(total_reward)
